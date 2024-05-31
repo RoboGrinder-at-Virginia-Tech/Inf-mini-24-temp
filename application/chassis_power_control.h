b9f0fbba1b123ce88e3e25308e0c9bf861ff2609
Zelin Shen <zshen25@vt.edu>
2024-05-31 01:39:40 -0400

    Update chassis_power_control.h

--- application/chassis_power_control.h
+++ application/chassis_power_control.h
@@ -222,5 +222,6 @@ typedef struct
   */
 extern void chassis_power_control_non_speed(chassis_move_t *chassis_power_control);
 extern void speed_adaptive_chassis_power_control(chassis_move_t *chassis_power_control);
-extern void superCap_speed_adaptive_chassis_power_control(chassis_move_t *chassis_power_control);
+extern void gen2_superCap_speed_adaptive_chassis_power_control(chassis_move_t *chassis_power_control);
+extern void gen3_superCap_speed_adaptive_chassis_power_control(chassis_move_t *chassis_power_control);
 #endif
